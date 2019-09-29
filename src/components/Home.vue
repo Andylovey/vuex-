f51c3831af19d3316a55bfba254096159b6bf4c9
xiqoqilin <w3c_2014@163.com>
2019-09-29 17:30:57 +0800

    完善vuex的异步操作

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,31 +1,47 @@
 <template>
   <div>
-    <h1>{{test}}</h1>
+    <h1>{{count}}</h1>
     <h1>{{student}}</h1>
-    <button @click="handleClick">click me</button>
+    <button @click="handleClickMutation">Mutation</button>
+
+    <button @click="handleClickAction">Action</button>
   </div>
 </template>
 
 <script>
-import { mapState, mapMutations } from "vuex";
+import { mapState, mapMutations, mapActions } from "vuex";
 
 export default {
   computed: {
-    ...mapState('HomeModules',{
-      test: state => state.test,
+    ...mapState("HomeModules", {
+      count: state => state.count,
       student: state => state.student
     })
   },
 
   methods: {
-    ...mapMutations('HomeModules',{
-      changeTest: 'changeTest',
-      changeStudent: 'changeStudent'
+    ...mapMutations("HomeModules", {
+      changeStudent: "changeStudent"
+    }),
+    ...mapActions('HomeModules',{
+      asyncChangeCount: 'asyncChangeCount'
     }),
 
-    handleClick() {
+    handleClickMutation() {
       this.changeStudent({
         value: 1
+      });
+    },
+
+    handleClickAction() {
+      // Promise接收第一种
+      // this.asyncChangeCount().then((res) => {
+      //   console.log(res)   // 执行结束
+      // })
+
+      // Promise接收第二种
+      Promise.all([this.asyncChangeCount()]).then((res) => {
+        console.log(res)   // ["执行结束"]
       })
     }
   }
--- src/model/Home/index.js
+++ src/model/Home/index.js
@@ -7,16 +7,47 @@ const HomeModule = {
             name: '麦迪',
             sex: '男',
             number: 175
-        }
+        },
+        count: 0
     },
-    
+
+    // 同步
     mutations: {
         changeStudent(state, payload) {
             return state.student = {
                 ...state.student,
                 number: payload.value
             }
+        },
+        changeCount(state, payload) {
+            return state.count = payload.count
         }
+    },
+
+    // 异步
+    actions: {
+        // 第一种
+        asyncChangeCount({ commit }) {
+            return new Promise((resolve,reject) => {
+                setTimeout(() => {
+                    // 异步改变状态值count 也可以传给页面'执行结束'
+                    commit('changeCount',{
+                        count: 123
+                    })
+                    resolve('执行结束')
+                },3000)
+            })
+        }
+
+        // 第二种(少用这种)
+        // asyncChangeCount({ commit }) {
+        //     setTimeout(async () => {
+        //         await commit('changeCount', {
+        //             count: 123
+        //         })
+        //         console.log('执行结束')
+        //     }, 3000)
+        // }
     }
 
 }
